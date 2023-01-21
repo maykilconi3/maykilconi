7d93638d92662b19dbfba4d8902cd34b19bcf7d6
maykilconi3 <123260616+maykilconi3@users.noreply.github.com>
2023-01-21 19:55:40 +0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php 
+//echo 'Daha güçlü olarak yakında online ';
+/*
+Sitenin her yeri sql injection ve xss dolu açmayın şuanlık fixleyelim öyle açarsınız.
+
+https://cdn.discordapp.com/attachments/921706439070859314/924056888734281808/unknown.png
+https://media.discordapp.net/attachments/921706439070859314/924056483346411520/unknown.png
+
+
+*/
+
+header('location: /login/');
